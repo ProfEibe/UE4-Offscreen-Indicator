d8f282f108bfc130a66f30da96ec85cb5b2bb617
Jakob Schade <jakob.schade@gmail.com>
2017-01-13 23:41:10 +0100

    First implementation with Lerp

--- Source/OffscreenIndicator/HUDBlueprintLibrary.cpp
+++ Source/OffscreenIndicator/HUDBlueprintLibrary.cpp
@@ -10,10 +10,13 @@ void UHUDBlueprintLibrary::FindScreenEdgeLocationForWorldLocation(UObject* World
 {
 	bIsOnScreen = false;
 	OutRotationAngleDegrees = 0.f;
-	FVector2D *ScreenPosition = new FVector2D();
+	FVector2D ScreenPosition = FVector2D();
 	
 	const FVector2D ViewportSize = FVector2D(GEngine->GameViewport->Viewport->GetSizeXY());
 	const FVector2D  ViewportCenter =  FVector2D(ViewportSize.X/2, ViewportSize.Y/2);
+
+	float m_invert = 1.0f;
+	float m_rotateInvert = 0.0f;
 	
 	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject);
 	
@@ -23,54 +26,42 @@ void UHUDBlueprintLibrary::FindScreenEdgeLocationForWorldLocation(UObject* World
 	ACharacter *PlayerCharacter = static_cast<ACharacter *> (PlayerController->GetPawn());
 	
 	if (!PlayerCharacter) return;
-	
-	
-	FVector Forward = PlayerCharacter->GetActorForwardVector();
-	FVector Offset = (InLocation - PlayerCharacter->GetActorLocation()).SafeNormal();
-	
-	float DotProduct = FVector::DotProduct(Forward, Offset);
-	bool bLocationIsBehindCamera = (DotProduct < 0);
-	
-	if (bLocationIsBehindCamera)
-	{
-		// For behind the camera situation, we cheat a little to put the
-		// marker at the bottom of the screen so that it moves smoothly
-		// as you turn around. Could stand some refinement, but results
-		// are decent enough for most purposes.
-		
-		FVector DiffVector = InLocation - PlayerCharacter->GetActorLocation();
-		FVector Inverted = DiffVector * -1.f;
-		FVector NewInLocation = PlayerCharacter->GetActorLocation() * Inverted;
-		
-		NewInLocation.Z -= 5000;
-		
-		PlayerController->ProjectWorldLocationToScreen(NewInLocation, *ScreenPosition);
-		ScreenPosition->Y = (EdgePercent * ViewportCenter.X) * 2.f;
-		ScreenPosition->X = -ViewportCenter.X - ScreenPosition->X;
-	}
-	
-	PlayerController->ProjectWorldLocationToScreen(InLocation, *ScreenPosition);
-	
-	// Check to see if it's on screen. If it is, ProjectWorldLocationToScreen is all we need, return it.
-	if (ScreenPosition->X >= 0.f && ScreenPosition->X <= ViewportSize.X
-		&& ScreenPosition->Y >= 0.f && ScreenPosition->Y <= ViewportSize.Y)
-	{
-		OutScreenPosition = *ScreenPosition;
-		bIsOnScreen = true;
-		return;
-	}
-	
-	*ScreenPosition -= ViewportCenter;
 
-	float AngleRadians = FMath::Atan2(ScreenPosition->Y, ScreenPosition->X);
+	FVector m_PlayerLocation = PlayerCharacter->GetActorLocation();
+	
+	PlayerController->ProjectWorldLocationToScreen(InLocation, ScreenPosition);
+	
+	if (ScreenPosition.X == 0.0f)
+	{
+		FVector location = FMath::Lerp(InLocation, m_PlayerLocation, 10.0f);
+		PlayerController->ProjectWorldLocationToScreen(InLocation, ScreenPosition);
+		m_invert = -1.0f;
+		m_rotateInvert = 180.0f;
+	}
+	else
+	{
+		// Check to see if it's on screen. If it is, ProjectWorldLocationToScreen is all we need, return it.
+		if (ScreenPosition.X >= 0 && ScreenPosition.X <= ViewportSize.X
+			&& ScreenPosition.Y >= 0 && ScreenPosition.Y <= ViewportSize.Y)
+		{
+			OutScreenPosition = ScreenPosition;
+			OutRotationAngleDegrees = 0.0f;
+			bIsOnScreen = true;
+			return;
+		}
+	}
+	
+	ScreenPosition -= ViewportCenter;
+
+	float AngleRadians = FMath::Atan2(ScreenPosition.Y, ScreenPosition.X);
 	AngleRadians -= FMath::DegreesToRadians(90.f);
 	
-	OutRotationAngleDegrees = FMath::RadiansToDegrees(AngleRadians) + 180.f;
+	OutRotationAngleDegrees = FMath::RadiansToDegrees(AngleRadians) + 180.f + m_rotateInvert;
 	
 	float Cos = cosf(AngleRadians);
 	float Sin = -sinf(AngleRadians);
 	
-	ScreenPosition = new FVector2D(ViewportCenter.X + (Sin * 150.f), ViewportCenter.Y + Cos * 150.f);
+	ScreenPosition = FVector2D(ViewportCenter.X + (Sin * 150.f), ViewportCenter.Y + Cos * 150.f); //Todo: check if necessary
 	
 	float m = Cos / Sin;
 	
@@ -78,24 +69,25 @@ void UHUDBlueprintLibrary::FindScreenEdgeLocationForWorldLocation(UObject* World
 	
 	if (Cos > 0)
 	{
-		ScreenPosition = new FVector2D(ScreenBounds.Y/m, ScreenBounds.Y);
+		ScreenPosition = FVector2D(ScreenBounds.Y/m, ScreenBounds.Y);
 	}
 	else
 	{
-		ScreenPosition = new FVector2D(-ScreenBounds.Y/m, -ScreenBounds.Y);
+		ScreenPosition = FVector2D(-ScreenBounds.Y/m, -ScreenBounds.Y);
 	}
 	
-	if (ScreenPosition->X > ScreenBounds.X)
+	if (ScreenPosition.X > ScreenBounds.X)
 	{
-		ScreenPosition = new FVector2D(ScreenBounds.X, ScreenBounds.X*m);
+		ScreenPosition = FVector2D(ScreenBounds.X, ScreenBounds.X*m);
 	}
-	else if (ScreenPosition->X < -ScreenBounds.X)
+	else if (ScreenPosition.X < -ScreenBounds.X)
 	{
-		ScreenPosition = new FVector2D(-ScreenBounds.X, -ScreenBounds.X*m);
+		ScreenPosition = FVector2D(-ScreenBounds.X, -ScreenBounds.X*m);
 	}
+
+	ScreenPosition *= m_invert;
+	ScreenPosition += ViewportCenter;
 	
-	*ScreenPosition += ViewportCenter;
-	
-	OutScreenPosition = *ScreenPosition;
+	OutScreenPosition = ScreenPosition;
 	
 }
